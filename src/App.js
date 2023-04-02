fb0d018653e2e8d4b26c476f5b2d6eb388c55fbb
Atharva <asphadnis1@gmail.com>
2023-04-02 19:15:07 +0530

    Add prod API URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import FeedLoading from "./components/feed/FeedLoading";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
-const apiUrl = process.env.API_URL ? process.env.API_URL : `http://127.0.0.1:8000/api/`;
+const apiUrl = process.env.API_URL
 
 function App() {
 
